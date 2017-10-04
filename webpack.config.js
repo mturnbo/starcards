8b42fcc75f289e6fd8d010570836aa667f38b6ab
Marcus Turnbo <marcus@emarcus.com>
2017-10-03 22:24:24 -0500

    Added basic App component

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const BUILD_DIR = path.resolve(__dirname, 'public');
 const APP_DIR = path.resolve(__dirname, 'src');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+	template: APP_DIR + '/index.html',
+	filename: 'index.html',
+	inject: 'body'
+});
 
 module.exports = {
   entry: APP_DIR + '/index.jsx',
@@ -9,6 +16,9 @@ module.exports = {
     path: BUILD_DIR,
     filename: 'bundle.js'
   },
+	plugins: [
+		HtmlWebpackPluginConfig
+	],
   module: {
     loaders: [
       {
