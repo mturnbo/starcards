6cff17af6b52cea036566e419c060565847e9eb1
Marcus Turnbo <mturnbo@gogoair.com>
2017-10-06 10:46:15 -0500

    Added tests

---
+++ src/components/Character/Character.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Character from './Character';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Character id={1}/>, div);
+});
